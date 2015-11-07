9eb8eba67d283e79bd04c4459e207be1271af7e2
Joshua Brain Jaffe-Pinckney <joshuabrainjaffe@gmail.com>
2015-11-07 16:10:36 -0500

    Reached MVP, albiet with many annoying little problems

--- JCMuralsApp/app/controllers/murals_controller.rb
+++ JCMuralsApp/app/controllers/murals_controller.rb
@@ -15,9 +15,25 @@ class MuralsController < ApplicationController
    @mural = Mural.new
  end
 
-  def create
+  # def create
+  #   @user = User.find_by(id: current_user.id)
+  #   @mural = Mural.create(mural_params, :users_id => @user.id)
+  #   redirect_to murals_path
+  # end
+
+def create
     @user = User.find_by(id: current_user.id)
-    @mural = Mural.create(mural_params, :users_id => @user.id)
+
+    title = mural_params[:title]
+    artist = mural_params[:artist]
+    website = mural_params[:website]
+    year = mural_params[:year]
+    description = mural_params[:description]
+    neighborhood = mural_params[:neighborhood]
+    address = mural_params[:address]
+    image = mural_params[:image]
+    
+    @mural = Mural.create(:title => title, :artist => artist, :website => website, :year => year, :description => description, :neighborhood => neighborhood, :address => address, :users_id => @user.id, :image => image)
     redirect_to murals_path
   end
 
@@ -44,8 +60,12 @@ class MuralsController < ApplicationController
   end
 
   def mural_params
-    params.require(:mural).permit(:title, :artist, :website, :year, :description, :neighborhood, :address, :image)
+      params.require(:mural).permit(:title, :artist, :website, :year, :description, :neighborhood, :address, :image)
   end
+
+  # def mural_params
+  #   params.require(:mural).permit(:title, :artist, :website, :year, :description, :neighborhood, :address, :image)
+  # end
 
   def proper_user
     mural = Mural.find(params[:id])
